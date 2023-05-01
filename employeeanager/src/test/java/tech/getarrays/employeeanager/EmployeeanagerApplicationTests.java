b665f3dbbda0cf846d90ae1152499f9f208ea08c
Felipe Pereira <95257377+felipepx@users.noreply.github.com>
2023-05-01 16:31:56 -0300

    add backend files

---
+++ employeeanager/src/test/java/tech/getarrays/employeeanager/EmployeeanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.getarrays.employeeanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
